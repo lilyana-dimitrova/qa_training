083bc90037dce83bd3834e752c32ee804a9a7b2a
lilyana-dimitrova <103064565+lilyana-dimitrova@users.noreply.github.com>
2022-04-07 18:21:58 +0300

    Lecture 4/ Task 7: Reverse list with for loop

---
+++ com.lecture4/src/Task7.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Task7 {
+    public static void main(String[] args) {
+
+        List<Integer> list1 = new ArrayList<>(
+                Arrays.asList(10, 20, 30, 40, 50));
+        System.out.println("Original order of list1: " + list1);
+
+        //Collections.reverse(list1);
+
+        for (int i = 0, j = list1.size() - 1; i < j; i++) {
+            list1.add(i, list1.remove(j));
+        }
+        System.out.println("Reversed order of list1: " + list1);
+    }
+
+}
+
+
+
