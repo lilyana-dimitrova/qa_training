e817a57f4e8ce1149cb736e7b12a5178a36dab01
lilyana-dimitrova <103064565+lilyana-dimitrova@users.noreply.github.com>
2022-04-13 13:18:51 +0300

    Lecture 5 / Task 2: print the elements of 2D array

---
+++ com.lecture5/src/Task2.java
@@ -0,0 +1,31 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[4][3];
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+        matrix[0][2] = 3;
+        matrix[1][0] = 4;
+        matrix[1][1] = 5;
+        matrix[1][2] = 6;
+        matrix[2][0] = 7;
+        matrix[2][1] = 8;
+        matrix[2][2] = 9;
+        matrix[3][0] = 6;
+        matrix[3][1] = 5;
+        matrix[3][2] = 4;
+
+        printMatrix(matrix);
+
+
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
