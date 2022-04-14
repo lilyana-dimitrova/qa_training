3edfac2a2e9388e1d76b5fbd5120febb3512b94f
lilyana-dimitrova <103064565+lilyana-dimitrova@users.noreply.github.com>
2022-04-14 17:55:46 +0300

    Lecture 5 / Task 7: Validate whether a given string is palindrome

---
+++ com.lecture5/src/Task7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+        String word;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word to check if palindrome: ");
+        word = scanner.next();
+        checkPalindrome(word);
+    }
+
+    public static boolean checkPalindrome(String word) {
+
+        StringBuilder reversed = new StringBuilder();
+        int wordSize = word.length();
+
+        for (int i = (wordSize - 1); i >=0; --i) {
+            reversed.append(word.charAt(i));
+        }
+
+        if (word.equalsIgnoreCase(reversed.toString())) {
+            System.out.println(word + " is a Palindrome String.");
+            return true;
+        }
+        else {
+            System.out.println(word + " is not a Palindrome String.");
+            return  false;
+        }
+    }
+
+
+}
