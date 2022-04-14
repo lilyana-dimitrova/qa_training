7f2243832bfaa4c9cc250627c89e5069b00d2a3c
lilyana-dimitrova <103064565+lilyana-dimitrova@users.noreply.github.com>
2022-04-14 19:00:32 +0300

    Lecture 6 / Task 4: Method to find if int is even

---
+++ com.lecture6/src/Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+    public static void main(String[] args) {
+
+        System.out.println(isEven(120));
+    }
+
+    public static boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+}
