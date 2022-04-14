8781a77a3e746552886938a0ecd71169e68353c3
lilyana-dimitrova <103064565+lilyana-dimitrova@users.noreply.github.com>
2022-04-14 19:11:28 +0300

    Lecture 6 / Task 5: Method to find if int is palindrome

---
+++ com.lecture6/src/Task5.java
@@ -0,0 +1,10 @@
+public class Task5 {
+
+    public static void main(String[] args) {
+        isPalindrome(4447);
+    }
+
+    public static void isPalindrome(int a) {
+        Task7.checkPalindrome(Integer.toString(a));
+    }
+}
